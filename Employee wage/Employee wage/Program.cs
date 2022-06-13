5a896513193c07eedbbf366944b31f79467f7861
Pratikbhandurge2022 <pratikbhandurge2222@gmail.com>
2022-06-13 18:13:27 +0530

    uc2

--- Employee wage/Employee wage/Program.cs
+++ Employee wage/Employee wage/Program.cs
@@ -18,15 +18,21 @@ namespace EmpWageComp
         {
             Console.WriteLine("Welcome to Employee Pay Roll");
             Random r = new Random();
+            int wagePerHour = 20;
+            int fullDayhour = 8;
+            int totalWage = 0;
             int number = r.Next(0, 2);
             if (number == 0)
             {
                 Console.WriteLine("Employee is Absent");
 
+
             }
             else
             {
                 Console.WriteLine("Employee is Present");
+                totalWage = wagePerHour * fullDayhour;
+                Console.WriteLine("Employee's total wage is:" + totalWage);
 
             }
         }
